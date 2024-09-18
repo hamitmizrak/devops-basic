157d527e7f62afdf20ffd3875919e28dd54a3018
Hamit Mızrak <hamitmizrak@outlook.com>
2024-09-18 11:55:38 +0300

    nginx

--- project_manuel.sh
+++ project_manuel.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
 
+echo "Nginx Docker Kurulumları"
+
+# Global Variable
+CLEANER="Temizleme"
+
 # Permission
 permission() {
-    chmod +x Counter.sh
+    chmod +x countdown.sh
+    chmod +x reboot.sh
 }
-permission
+#permission
+
+# Clean
+clean(){
+    sleep 2
+    echo -e "\n#### ${CLEANER} ####"
+    read -p "Sistemdeki Gereksiz Dosyaların ve Bağımlılıklarını Temizlemek İster misiniz ? e/h " cleanResult
+    if [[ $cleanResult == "e" || $cleanResult == "E" ]]; then
+        echo -e "Gereksiz Paketlerden arta kalan veriler siliniyor..."
+
+        # Geriye Say
+        ./countdown.sh
+
+        echo -e "#### Cleaning ####\n"
+        apt-get autoremove -y
+        apt autoclean
+
+        echo -e "#### Gerekli Bağımlılıkları Yükleniyor ####\n"
+        apt install -f
+    else
+        echo -e "Temizleme İşlemi Gerçekleşmedi"
+    fi        
+}
+#clean
 
 # Docker Compose
 nginxDocker() {
+    echo -e "Docker başlıyor...."
     # Eğer defaultta: daocker-compose.yml ise
     # docker-compose up
     # docker-compose up -d
 
     # Nginx-docker-compose.yml
-    docker-compose -f Nginx-docker-compose.yml up
     #docker-compose -f Nginx-docker-compose.yml up -d
+    docker-compose -f Nginx-docker-compose.yml up
 }
 nginxDocker
